d8cacb98dcd1c474b472b7abff6df066ce170ea9
Reagz <44364513+Reagz90@users.noreply.github.com>
2023-08-06 21:33:35 +1000

    UI design

---
+++ src/components/Information.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Translation from "./Translation";
+import Transcription from "./Transcription";
+
+export default function Information() {
+  const [tab, setTab] = useState("transcription");
+
+  return (
+    <div>
+      <main className="flex-1 p-4 flex flex-col text-center gap-3 sm: gap-4 md: gap-5 justify-center items-center h-screen pb-20 w-fit max-w-prose w-full mx-auto">
+        <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap">
+          Your <span className="text-blue-400 bold">Transcription</span>
+        </h1>
+        <div className="grid grid-cols-2 items-center mx-auto bg-white shadow rounded-full overflow-hidden">
+          <button
+            onClick={() => setTab("transcription")}
+            className={
+              "px-4 duration-200 py-1 font-medium" +
+              (tab === "transcription"
+                ? " bg-blue-400 text-white"
+                : " text-blue-400 hover:text-blue-600")
+            }
+          >
+            Transcription
+          </button>
+          <button
+            onClick={() => setTab("translation")}
+            className={
+              "px-4 duration-200 py-1 font-medium" +
+              (tab === "translation"
+                ? " bg-blue-400 text-white"
+                : " text-blue-400 hover:text-blue-600")
+            }
+          >
+            Translation
+          </button>
+        </div>
+        {tab === "transcription" ? <Transcription /> : <Translation />}
+      </main>
+    </div>
+  );
+}
